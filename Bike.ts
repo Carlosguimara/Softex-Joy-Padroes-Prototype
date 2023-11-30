6a92b35a2ffca1ac521a987e035fa4b78c9a63fa
Carlosguimara <139878571+Carlosguimara@users.noreply.github.com>
2023-11-30 18:46:11 -0300

    Add files via upload

---
+++ Bike.ts
@@ -0,0 +1,24 @@
+import { Veiculo } from "./Veiculo";
+export class Bike extends Veiculo {
+    
+    private _numeroMarchas: number;
+    
+    constructor(modelo: string, marca: string, cor: string, numeroRodas: number, numeroMarchas: number) {
+        super(modelo, marca, cor, numeroRodas);
+        this._numeroMarchas = numeroMarchas;
+    }
+
+    public represent(): string {
+        return `modelo: ${this.modelo} - marca: ${this.marca} - cor: ${this.cor} - rodas: ${this.numeroRodas} - marchas: ${this.numeroMarchas}`;
+    }
+
+    // getters e setters
+    get numeroMarchas(): number {
+        return this._numeroMarchas;
+    }
+    set numeroMarchas(val: number) {
+        this._numeroMarchas = val;
+    }
+    
+
+}
